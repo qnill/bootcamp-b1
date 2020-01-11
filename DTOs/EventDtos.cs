911e185e282a84028582598d0adc4960a8c474c4
Anıl Yıldırım <anilyildirim12@gmail.com>
2020-01-11 15:36:23 +0300

    Added event Dtos and Model

---
+++ DTOs/EventDtos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace net_core_bootcamp_b1.DTOs
+{
+    public class EventAddDto
+    {
+        [Required, MaxLength(250)]
+        public string Name { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime FinishDate { get; set; }
+        [Required]
+        public string Address { get; set; }
+        public bool IsFree { get; set; }
+        public double Price { get; set; }
+        public string Subject { get; set; }
+        public string Desc { get; set; }
+    }
+
+    public class EventUpdateDto : EventAddDto
+    {
+        [Required]
+        public Guid Id { get; set; }
+    }
+
+    public class EventGetDto
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime FinishDate { get; set; }
+        public string Address { get; set; }
+        public bool IsFree { get; set; }
+        public double Price { get; set; }
+        public string Subject { get; set; }
+        public string Desc { get; set; }
+    }
+}
---
+++ Models/Event.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace net_core_bootcamp_b1.Models
+{
+    public class Event
+    {
+        public Guid Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        [Required, MaxLength(250)]
+        public string Name { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime FinishDate { get; set; }
+        [Required]
+        public string Address { get; set; }
+        public bool IsFree { get; set; }
+        public double Price { get; set; }
+        public string Subject { get; set; }
+        public string Desc { get; set; }
+    }
+}
