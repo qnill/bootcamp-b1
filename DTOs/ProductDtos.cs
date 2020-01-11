9b01f2c51b46e4cf6d96be5a374c3994560cc110
Anıl Yıldırım <anilyildirim12@gmail.com>
2020-01-11 13:39:18 +0300

    Lesson-2 : Add dtos and model to product.

---
+++ DTOs/ProductDtos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace net_core_bootcamp_b1.DTOs
+{
+    public class ProductAddDto
+    {
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class ProductUpdateDto
+    {
+        public Guid? Id { get; set; }
+        public string Name { get; set; }
+        public string Desc { get; set; }
+    }
+
+    public class ProductGetDto
+    {
+        public Guid? Id { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public double Price { get; set; }
+    }
+}
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 
-namespace net_core_bootcamp_b1
+namespace net_core_bootcamp_b1.Models
 {
     public class Product
     {
         public Guid? Id { get; set; }
+        public bool IsDeleted { get; set; }
         public DateTime? CreatedAt { get; set; }
         public string Name { get; set; }
         public string Desc { get; set; }
